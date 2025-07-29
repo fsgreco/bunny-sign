6bee9f150b117de28bb3fab3e3dfea767a8a8377
Santiago <fsgreco@fastmail.com>
2025-07-29 16:25:35 +0200

    fix: :adhesive_bandage: ignore formatting delay function

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,5 @@ export const random = (min, max) => Math.floor(Math.random() * (max - min) + min
 
 export const pickRandom = arr => arr[random(0, arr.length - 1)]
 
-export const delay = ms =>
-	new Promise(res => {
-		setTimeout(res, ms)
-	})
+// prettier-ignore
+export const delay = ms => new Promise(resolve => { setTimeout(resolve, ms) })
