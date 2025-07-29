aa47b089f400593720b8d4512d218e0614224901
Santiago <fsgreco@fastmail.com>
2025-07-29 10:28:07 +0200

    feat: implement bunny sign generator

---
+++ src/bunny-sign.js
@@ -0,0 +1,57 @@
+// @ts-check
+import { useAscii } from './utils.js'
+
+/*
+|￣￣￣￣￣￣￣￣￣￣￣|
+    Bunny Sign 
+|＿＿＿＿＿＿＿＿＿＿＿|
+  (\__/) ||
+  (•ㅅ•) ||
+  /    づ
+*/
+
+export const eyes = useAscii ? ['•', 'o', '•', 'O', '^', '•'] : ['●', '○', '•', "'", '^']
+
+export const mouths = useAscii ? ['*'] : ['ㅅ']
+
+/* Extended sets */
+/* ['*' '•', 'O', 'o', 'o', '•', '-' ] */
+/* ['ㅅ' '•', '○', '■', '▪', '▫', '▬', '▭', '-', '○' ] */
+
+/**
+ * Generates a frame of the bunny sign
+ * @param {string} msg The message for the sign
+ * @param {{mouth?: string, eye?: string}} param1 optional configuration object
+ * @returns {string}
+ */
+export const generateBunnySign = (msg, { mouth = mouths[0], eye = eyes[0] } = {}) => {
+	const pad = ' '.repeat(5)
+
+	const addNewLines = msg => {
+		let count = 0
+		const msgWithNewLines = msg.split(' ').reduce(
+			(accWords, word) => {
+				count = count + word.length + 1
+				if (count > 13) {
+					count = 0
+					return [...accWords, `\n${pad}`, word]
+				}
+				return [...accWords, word]
+			},
+			[pad]
+		)
+		return msgWithNewLines.join(' ')
+	}
+
+	const message = addNewLines(msg)
+
+	const eme = `${eye}${mouth}${eye}`
+	return [
+		` |￣￣￣￣￣￣￣￣￣￣￣|`,
+		`${message}`,
+		` |＿＿＿＿＿＿＿＿＿＿＿|`,
+		`   (\\__/) ||`,
+		`   (${eme}) ||`,
+		`   /    づ`,
+	].join('\n')
+}
